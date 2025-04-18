e1cbf2d6e4671400f07fd1d5e783beed528a2409
Mohammad Ali Taqvazadeh <m.a.taqvazadeh@gmail.com>
2025-04-18 12:23:48 +0330

    Update two sum sample

--- session11/samples/two-sum.py
+++ session11/samples/two-sum.py
@@ -5,17 +5,19 @@
 # target = 8
 # [1,2,5,6]
 
+
 # Time = n + n + n + n + ... + n = n * n = n ^ 2 = O(n^2)
 # Space = O(1)
 def first(arr: list[int], target: int) -> bool:
-    for index1 in range(len(arr)):
-        for index2 in range(len(arr)):
+    for index1, member1 in enumerate(arr):
+        for index2, member2 in enumerate(arr):
             if index1 == index2:
                 continue
-            
-            if arr[index1] + arr[index2] == target:
+
+            if member1 + member2 == target:
                 return True
     return False
+
 
 # Time = O(n)
 # Space = O(n)
@@ -28,6 +30,7 @@ def second(arr: list[int], target: int) -> bool:
         else:
             s.add(member)
     return False
+
 
 def main():
     arr = [0, -1, 2, -3, 1]
@@ -42,14 +45,17 @@ def main():
     result = second(arr, target)
     assert result == False
 
+
 def benchmark() -> tuple:
     n = 1_000_000
-    
+
     import random
+
     arr = random.sample(range(-1_000_000, 1_000_000), n)
     target = arr[-1] + arr[-2]
 
     import time
+
     # print('First Algorithm = O(n^2)')
     start = time.time()
     result = first(arr, target)
@@ -69,15 +75,13 @@ def benchmark() -> tuple:
     return (execution1, execution2)
 
 
-
-if __name__ == '__main__':
-    #main()
+if __name__ == "__main__":
+    # main()
     sum1, sum2 = 0, 0
     for _ in range(1000):
         result = benchmark()
         sum1 += result[0]
         sum2 += result[1]
 
-    print(f'Execution time first = {(sum1 / 1000) * 1000} ms')
-    print(f'Execution time second = {(sum2 / 1000) * 1000} ms')
-
+    print(f"Execution time first = {(sum1 / 1000) * 1000} ms")
+    print(f"Execution time second = {(sum2 / 1000) * 1000} ms")
